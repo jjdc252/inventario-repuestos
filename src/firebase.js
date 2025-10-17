388bb8efb665eff01d089841983fcc1714d5cccf
Julian Duran <jjdc252003@gmail.com>
2025-10-16 23:35:10 -0500

    Actualizar firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuni3fmaQAPiE61OuDP8A6s4qybDrGelU",
-  authDomain: "buscador-canastillas-e9352.firebaseapp.com",
-  projectId: "buscador-canastillas-e9352",
-  storageBucket: "buscador-canastillas-e9352.firebasestorage.app",
-  messagingSenderId: "463137837323",
-  appId: "1:463137837323:web:5dfc9611a4983aa8b2d20e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBuni3fmaQAPiE61OuDP8A6s4qybDrGelU",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "buscador-canastillas-e9352.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "buscador-canastillas-e9352",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "buscador-canastillas-e9352.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "463137837323",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:463137837323:web:5dfc9611a4983aa8b2d20e"
 };
 
 const app = initializeApp(firebaseConfig);
